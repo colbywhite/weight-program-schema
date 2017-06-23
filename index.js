20bc9b01b709e0820e59103375a397c6c6c17669
Colby M. White <colbywhite@users.noreply.github.com>
2017-06-22 21:20:18 -0500

    adding a mwf calculator

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const candito_squat = require('./lib/candito_squat')
 const stronglifts = require('./lib/stronglifts')
+const schedule_calculator = require('./lib/schedule_calculator')
 
 module.exports = {
+  schedule_calculator: schedule_calculator,
   programs: [candito_squat, stronglifts],
   _schema: require('./lib/weight-program-schema')
 }
